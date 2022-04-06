77e097d376cddf7d52a2b3174c3ff6effad5d25a
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-04-06 12:35:42 -0500

    Outlined the ToggleButtonEventHandler class

--- src/handlers/ToggleButtonEventHandler.java
+++ src/handlers/ToggleButtonEventHandler.java
@@ -1,5 +1,21 @@
 package handlers;
 
+import java.awt.event.ActionEvent;
+
+import models.NonogramMakerModel;
+
 public class ToggleButtonEventHandler {
+	
+	private NonogramMakerModel model;
+	private int rowIdx;
+	private int colIdx;
+	
+	public ToggleButtonEventHandler(NonogramMakerModel model, int rowIdx, int colIdx) {
+		
+	}
+	
+	public void Handle(ActionEvent event) {
+		
+	}
 
 }
