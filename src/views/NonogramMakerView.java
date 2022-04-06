9220476e519715ef6480049302b528308aeb84c9
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-04-06 12:25:19 -0500

    Outlined the NonogramMakerView class

---
+++ src/views/NonogramMakerView.java
@@ -0,0 +1,48 @@
+package views;
+
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.util.HashMap;
+
+public class NonogramMakerView {
+	
+	private BorderPane borderPane;
+	private MenuBar menubar;
+	private CellGridView cellGridView;
+	private HashMap<String, MenuItem> menuItemsMap = new HashMap<String, MenuItem>();
+	public String MENU_ITEM_OPEN = "MENU_ITEM_OPEN";
+	public String MENU_ITEM_SAVE = "MENU_ITEM_SAVE";
+	public String MENU_ITEM_EXIT = "MENU_ITEM_EXIT";
+	
+	public NonogramMakerView(int numRows, int numCols, int cellLength) {
+		
+	}
+	private void initMenuBar() {
+		
+	}
+	
+	public MenuItem getMenuItem(String name) {
+		
+	}
+	
+	public Pane getPane() {
+		
+	}
+	
+	public void initButtons(int numRows, int numCols, int cellLength) {
+		
+	}
+	
+	public ToggleButton getToggleButton(int row, int col) {
+		
+	}
+	
+	public int getNumRow() {
+		
+	}
+	
+	public int getNumCols() {
+		
+	}
+
+}
