5a96e1d4dd59490f753b857af13baffda070188d
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-04-06 12:32:09 -0500

    Outlined the CellGridView class

--- src/views/CellGridView.java
+++ src/views/CellGridView.java
@@ -1,5 +1,36 @@
 package views;
 
+import java.util.ArrayList;
+
 public class CellGridView {
+	
+	private ArrayList<ToggleButton> gridButtons = new ArrayList<ToggleButton>();
+	private GridPane gridPane;
+	private int numRows;
+	private int numCols;
+	
+	public CellGridView(int numRows, int numCols, int cellLength) {
+		
+	}
+	
+	public void initButtons(int numRows, int numCols, int cellLength) {
+		
+	}
+	
+	public int getNumRows() {
+		
+	}
+	
+	public int getNumCols() {
+		
+	}
+	
+	public ToggleButton getToggleButtons(int row, int col) {
+		
+	}
+	
+	public Pane getPane() {
+		
+	}
 
 }
