2f5217cb7c4d4b5ed94b4a1fd608f972e2222247
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-04-07 09:57:45 -0500

    fixed the presenter class

--- src/edu/ou/cs2334/project4/Presenter/NonogramMakerPresenter.java
+++ src/edu/ou/cs2334/project4/Presenter/NonogramMakerPresenter.java
@@ -1,12 +1,12 @@
-package Presenter;
+package edu.ou.cs2334.project4.Presenter;
 
 import java.awt.Window;
 import java.io.File;
 
-import interfaces.Openable;
-import interfaces.Saveable;
-import models.NonogramMakerModel;
-import views.NonogramMakeView;
+import edu.ou.cs2334.project4.interfaces.Openable;
+import edu.ou.cs2334.project4.interfaces.Saveable;
+import edu.ou.cs2334.project4.models.NonogramMakerModel;
+import edu.ou.cs2334.project4.views.NonogramMakeView;
 
 public class NonogramMakerPresenter implements Saveable, Openable {
 	
