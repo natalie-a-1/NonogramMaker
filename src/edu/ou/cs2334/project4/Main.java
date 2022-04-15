2b4fc14841546acc1a55ee7e5537b43f40f7f53a
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-04-15 11:21:37 -0500

    Outlined the Main class

--- src/edu/ou/cs2334/project4/Main.java
+++ src/edu/ou/cs2334/project4/Main.java
@@ -1,17 +1,32 @@
 package edu.ou.cs2334.project4;
 
+import edu.ou.cs2334.project4.Presenter.NonogramMakerPresenter;
+import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 	
-	private static int IDX_NUM_COLS = 0;
-	private static int IDX_NUM_ROWS = 1;
+	private static int IDX_NUM_COLS = 1;
+	private static int IDX_NUM_ROWS = 0;
 	private static int IDX_CELL_SIZE = 2;
 
 	public static void main(String[] args) {
-		
-		public void start(Stage primaryStage;
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO NEEDS TO BE FIXED 
+		getParameters();
+		NonogramMakerPresenter nonogramMakerPresenter = new NonogramMakerPresenter(10,10,10);
+		Scene scene = new Scene(nonogramMakerPresenter.getPane());
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		scene.getStylesheets().add("style.css");
+		stage.setTitle("My first java app!");
+		stage.setResizable(false);
+		stage.show();
 	}
 
 }
